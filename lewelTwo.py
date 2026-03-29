b621139a69110d23dd2ccf82414a530c759ac905
DonutKnight <dominik200310@gmail.com>
2026-03-29 12:10:17 +0200

    Poprawa błedów i dodanie requirements.txt

--- lewelTwo.py
+++ lewelTwo.py
@@ -3,6 +3,5 @@ MAP_PATH = "maps/LewelTwo.tmx"
 
 GROUND_LAYERS = ["ruch", "poruszanie"]
 TRAP_LAYERS = ["pulapki", "spikes", "kolce"]
-
 # Nazwa następnego poziomu (zapętlamy do jedynki lub zostawiamy puste)
 NEXT_LEVEL_NAME = "lewelThree"

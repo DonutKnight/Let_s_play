757fd8c0a45763bd888f3cadc71c2f9c0535e4d4
sirStanley <staszek.mikolajek@gmail.com>
2026-03-30 20:02:01 +0200

    dodanie plików gry i nowej planszy z bossem

---
+++ lewelOne.py
@@ -0,0 +1,10 @@
+
+
+MAP_PATH = "maps/LewelOne.tmx"
+
+
+GROUND_LAYERS = ["ruch", "poruszanie"]
+TRAP_LAYERS = ["pulapki", "spikes", "kolce"]
+
+# Nazwa następnego poziomu
+NEXT_LEVEL_NAME = "lewelTwo"

e492c24101643fb9b5dd437b3f523f65485462a2
SirStanley <92999490+SirStanley@users.noreply.github.com>
2026-04-03 11:07:20 +0200

    zmiana walki z bossem i punktacja

--- lewelOne.py
+++ lewelOne.py
@@ -6,9 +6,5 @@ MAP_PATH = "maps/LewelOne.tmx"
 GROUND_LAYERS = ["ruch", "poruszanie"]
 TRAP_LAYERS = ["pulapki", "spikes", "kolce"]
 
-<<<<<<< HEAD
 # Nazwa następnego poziomu
-=======
-# Nazwa następnego poziomu (do wykorzystania później)
->>>>>>> 509edc0f749590efdffaf0fdd0702861a5fc9097
 NEXT_LEVEL_NAME = "lewelTwo"
